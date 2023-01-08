6828bc2406d5f98a2f9eb26d735549a929462627
Jonny Bylsma <jmbylsma@gmail.com>
2023-01-08 10:36:49 -0500

    Improve layout spacing

--- keyboards/kinesis/keymaps/jbylsma/keymap.c
+++ keyboards/kinesis/keymaps/jbylsma/keymap.c
@@ -1,9 +1,13 @@
 #include QMK_KEYBOARD_H
 
+#define VOL_M KC_KB_MUTE
+#define VOL_D KC_KB_VOLUME_DOWN
+#define VOL_U KC_KB_VOLUME_UP
+
 enum {
-  _BASE,
-  _GAME,
-  _PROGM,
+  _BS, // Base
+  _GA, // Game
+  _PG, // Progrm
 };
 
 void _set_layer_led(layer_state_t state);
@@ -35,27 +39,27 @@ void _set_layer_led(layer_state_t state);
 */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_BASE] = LAYOUT(
-           KC_ESC ,KC_F1  ,KC_F2  ,KC_F3   ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8,
-           KC_EQL ,KC_1   ,KC_2   ,KC_3    ,KC_4   ,KC_5   ,
-           KC_TAB ,KC_Q   ,KC_W   ,KC_E    ,KC_R   ,KC_T   ,
-           KC_CAPS,KC_A   ,KC_S   ,KC_D    ,KC_F   ,KC_G   ,
-           KC_LSFT,KC_Z   ,KC_X   ,KC_C    ,KC_V   ,KC_B   ,
-                   KC_GRV ,KC_INS ,KC_LEFT ,KC_RGHT,
-                                   KC_LCTL ,KC_LALT,
-                                            KC_LGUI,
-                           KC_BSPC,KC_DEL  ,KC_PGUP,
-           KC_F9  ,KC_F10 ,KC_F11 ,KC_F12  ,KC_KB_MUTE ,KC_KB_VOLUME_DOWN  ,KC_KB_VOLUME_UP, KC_NO, MO(_PROGM),
-           KC_6   ,KC_7   ,KC_8   ,KC_9    ,KC_0   ,KC_MINS,
-           KC_Y   ,KC_U   ,KC_I   ,KC_O    ,KC_P   ,KC_BSLS,
-           KC_H   ,KC_J   ,KC_K   ,KC_L    ,KC_SCLN,KC_QUOT,
-           KC_N   ,KC_M   ,KC_COMM,KC_DOT  ,KC_SLSH,KC_RSFT,
-                   KC_DOWN,KC_UP  ,KC_LBRC ,KC_RBRC,
-                           KC_RALT,KC_RCTL ,
+[_BS] = LAYOUT(
+           KC_ESC ,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8  ,
+           KC_EQL ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,
+           KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,
+           KC_CAPS,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,
+           KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,
+                   KC_GRV ,KC_INS ,KC_LEFT,KC_RGHT,
+                                   KC_LCTL,KC_LALT,
+                                           KC_LGUI,
+                           KC_BSPC,KC_DEL ,KC_PGUP,
+           KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,VOL_M  ,VOL_D  ,VOL_U  ,KC_NO  ,MO(_PG),
+           KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_MINS,
+           KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_BSLS,
+           KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT,
+           KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
+                   KC_DOWN,KC_UP  ,KC_LBRC,KC_RBRC,
+                           KC_RALT,KC_RCTL,
                            KC_RGUI,
-                           KC_PGDN,KC_ENTER,KC_SPC
+                           KC_PGDN,KC_ENT ,KC_SPC
     ),
-[_GAME] = LAYOUT(
+[_GA] = LAYOUT(
            _______,_______,_______,_______,_______,_______,_______,_______,_______,
            _______,_______,_______,_______,_______,_______,
            _______,_______,_______,_______,_______,_______,
@@ -75,25 +79,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                            _______,
                            _______,_______,KC_BSPC
     ),
-[_PROGM] = LAYOUT(
-           _______  ,_______,_______,_______,_______,_______,_______,_______,_______,
-           _______  ,_______,_______,_______,_______,_______,
-           _______  ,_______,_______,_______,_______,_______,
-           DF(_BASE),_______,_______,_______,_______,DF(_GAME),
-           _______  ,_______,_______,_______,_______,_______,
-                     _______,_______,_______,_______,
-                                     _______,_______,
-                                             _______,
-                             _______,_______,_______,
-           QK_BOOT  ,_______,_______,_______,_______,_______,_______,_______,_______,
-           _______  ,_______,_______,_______,_______,_______,
-           _______  ,_______,_______,_______,_______,_______,
-           _______  ,_______,_______,_______,_______,_______,
-           _______  ,_______,_______,_______,_______,_______,
-                     _______,_______,_______,_______,
-                        _______,_______,
-                        _______,
-                        _______,_______,_______
+[_PG] = LAYOUT(
+           _______,_______,_______,_______,_______,_______,_______,_______,_______,
+           _______,_______,_______,_______,_______,_______,
+           _______,_______,_______,_______,_______,_______,
+           DF(_BS),_______,_______,_______,_______,DF(_GA),
+           _______,_______,_______,_______,_______,_______,
+                   _______,_______,_______,_______,
+                                   _______,_______,
+                                           _______,
+                           _______,_______,_______,
+           QK_BOOT,_______,_______,_______,_______,_______,_______,_______,_______,
+           _______,_______,_______,_______,_______,_______,
+           _______,_______,_______,_______,_______,_______,
+           _______,_______,_______,_______,_______,_______,
+           _______,_______,_______,_______,_______,_______,
+                   _______,_______,_______,_______,
+                           _______,_______,
+                           _______,
+                           _______,_______,_______
     ),
 };
 
