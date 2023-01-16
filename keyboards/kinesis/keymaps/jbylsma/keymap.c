fac8141db98564c7a5b576d6ca8d685c948017f3
Jonny Bylsma <jmbylsma@gmail.com>
2023-01-15 22:28:42 -0500

    Swap Esc/Caps Lock at keyboard level

--- keyboards/kinesis/keymaps/jbylsma/keymap.c
+++ keyboards/kinesis/keymaps/jbylsma/keymap.c
@@ -42,10 +42,10 @@ layer_state_t _set_layer_led(layer_state_t state);
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BS] = LAYOUT(
-           KC_ESC ,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8  ,
+           KC_CAPS,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8  ,
            KC_EQL ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,
            KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,
-           KC_CAPS,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,
+           KC_ESC ,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,
            KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,
                    KC_GRV ,MO(_PG),KC_LEFT,KC_RGHT,
                                    KC_LCTL,KC_LALT,
