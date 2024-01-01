631baadfa44ad1a98a8594fb9defe814bd3019e9
Jonny Bylsma <jmbylsma@gmail.com>
2023-12-31 23:49:42 -0500

    0.22 branch compatibility tweaks

--- keyboards/kinesis/keymaps/jbylsma/rules.mk
+++ keyboards/kinesis/keymaps/jbylsma/rules.mk
@@ -8,3 +8,7 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight. 
 COMBO_ENABLE = yes
+
+# https://github.com/qmk/qmk_firmware/pull/21353 causes keyboards not to load correctly
+# https://github.com/kinx-project/kint/issues/77#issuecomment-1837631133
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=FALSE
